19d32cdb0c7d630f60410ab10447889ca0459e87
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-09-02 12:18:09 +0300

    show form

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("sdfsdfsdfdsfsd");
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
