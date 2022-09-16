7d0b58a55dbf6fcfe2624ab206a4587912e3a327
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-09-16 18:28:32 +0300

    rendering projects list

--- dist/app.js
+++ dist/app.js
@@ -36,6 +36,27 @@ function autobind(target, methodName, descriptor) {
     };
     return adjDescriptor;
 }
+//ProjectList Class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type.toUpperCase() + 'PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 // ProjectInputClass
 class ProjectInput {
     constructor() {
@@ -104,3 +125,5 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
